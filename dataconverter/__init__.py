3a70195e37b97f0c31745730c6e93ec68479ab89
jacek-komar <jacekpaw2002@gmail.com>
2023-08-08 14:23:46 +0200

    Pierwsza próba wrzucenia czegoikolwiek

---
+++ dataconverter/__init__.py
@@ -0,0 +1,14 @@
+
+#Nazwy obiektów spoza diagramu klas na razie robocze (nie mogę wygrzebać dokumentacji z nazwami)
+
+class DataValidator:
+    def __init__(self):
+        print("Witam")
+
+    def __del__(self):
+        print("No na razie")
+
+    def serialize(databaseQuery):
+
+
+
