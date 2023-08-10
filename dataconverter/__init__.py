14b47cf742a6e18153047f7638aeeb47bfd724dd
jacek-komar <jacekpaw2002@gmail.com>
2023-08-10 13:58:18 +0200

    Done: sending queries to test database

--- dataconverter/__init__.py
+++ dataconverter/__init__.py
@@ -1,16 +1,43 @@
 
-#Nazwy obiektów spoza diagramu klas na razie robocze (nie mogę wygrzebać dokumentacji z nazwami)
+'''Nazwy obiektów spoza diagramu klas na razie robocze (nie mogę wygrzebać dokumentacji z nazwami)
+'''
 
-class DataValidator:
+import sqlite3
+import json
+import datetime
+
+json data = {
+    "message":{
+        "mode": "PUT",
+        "timestamp": datetime.date.now(),
+        "id": 1,
+        "uder_id":1,
+        "content": "Well I tell you what you fat little cunt"
+    }
+}
+
+text = "Dobazy"
+
+class DataConverter:
     def __init__(self):
         print("Witam")
 
     def __del__(self):
         print("No na razie")
 
-    def serialize(databaseQuery):
-        for #find how to make for loop looking for a character and splitting the strig at that point
+    '''def deserialize(data):'''
+    def sendToDatabase(text):
+        con = sqlite3.connect("test.db")
+        cur = con.cursor()
+        cur.execute("""
+            INSERT INTO dupa VALUES
+            ('tak','nie',20),
+            ('nie','tak',15)
+        """)
+        con.commit()
 
+klasa1 = DataConverter()
 
+klasa1.sendToDatabase()
 
 
